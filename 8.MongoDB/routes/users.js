e4344a6e6fe6985161a6d7a1f11b51afe1f6c92a
현석 송 <jhn06046@hotmail.com>
2021-02-24 17:49:42 +0900

    오류 수정

--- 8.MongoDB/routes/users.js
+++ 8.MongoDB/routes/users.js
@@ -5,6 +5,7 @@ const Comment = require('../schemas/comment');
 const router = express.Router();
 
 router.route('/')
+    // 유저를 찾는 부분
     .get(async (req,res,next)=>{
         try{
             const users = await User.find({});
@@ -15,8 +16,10 @@ router.route('/')
             next(err);
         }
     })
+    // 유저 생성 부분 -> Post요청
     .post(async (req,res,next)=>{
         try{
+            // 몽고db에서는 save이지만 몽구스에서는 create로 생성
             const user = await User.create({
                 name: req.body.name,
                 age: req.body.age,
@@ -30,10 +33,11 @@ router.route('/')
             next(err);
         }
     });
-
+    // 유저를 클릭했을 때 해당 유저의 댓글을 가져오는 함수
     router.get('/:id/comments',async(req,res,next)=>{
         try{
             const comments = await Comment.find({ commenter: req.params.id })
+            // popluate는
                 .populate('commenter');
             console.log(comments);
             res.json(comments);
